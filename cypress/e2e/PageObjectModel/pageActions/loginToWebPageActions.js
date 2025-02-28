32c6293a95592b572cab8241de7ec7bc1136108d
Janani A <jananik1029@gmail.com>
2025-02-28 23:00:35 +0530

    fix for uncaught exception

--- cypress/e2e/PageObjectModel/pageActions/loginToWebPageActions.js
+++ cypress/e2e/PageObjectModel/pageActions/loginToWebPageActions.js
@@ -6,7 +6,7 @@ class loginBankActions{
 
     navigateToLoginBank(){
           cy.visit(elements.visit)
-          cy.xpath(elements.checkPageHeader).should('have.title',"GlobalSQA")
+          cy.xpath(elements.checkPageHeader).should('include.text', 'GlobalSQA')
         //   cy.xpath(elements.checkBankName).should('have.text',"XYZ Bank")
           cy.xpath(elements.clickTesthub).trigger('mouseover').wait(1000)
     }
--- cypress/e2e/PageObjectModel/pageElements/loginElements.js
+++ cypress/e2e/PageObjectModel/pageElements/loginElements.js
@@ -1,6 +1,6 @@
 module.exports={
     visit:"https://www.globalsqa.com/",
-    checkPageHeader:"//a[@rel='home']",
+    checkPageHeader:"//div[@id='logo']//a",
     checkBankName:"//strong[@class='mainHeading']",
     clickTesthub:"//div[@id='menu']//a[text()='Tester’s Hub']"
 }
